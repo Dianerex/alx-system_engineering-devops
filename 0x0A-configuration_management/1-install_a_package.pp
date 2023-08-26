39e2723858f4dcb24b5235d6c4673560662b4c1c
Dianerex <dianerex@gmail.com>
2023-08-25 17:51:03 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install puppet-lint -v 2.5.0
+# Define a package resource to install pip3
+package { 'python3-pip':
+  ensure => installed,
+}
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Define an exec resource to install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/lib/python3.x/dist-packages/flask', # Adjust the path as needed
+  require => Package['python3-pip'], # Make sure pip3 is installed first
 }
