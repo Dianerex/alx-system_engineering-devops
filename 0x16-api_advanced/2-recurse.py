0e769f07c4fc95de0193d5f9c2edc0db7478e25d
root <zinnyrex@yahoo.com>
2023-10-10 14:26:53 -0700

    0x16. API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,38 @@
-#!/usr/bin/python3
-"""Module for task 2"""
+import requests
 
+def get_hot_posts(subreddit, hot_list=[], count=0, after=None):
+    """
+    Queries the Reddit API and returns all hot posts of the subreddit.
 
-def recurse(subreddit, hot_list=[], count=0, after=None):
-    """Queries the Reddit API and returns all hot posts
-    of the subreddit"""
-    import requests
+    :param subreddit: The name of the subreddit to retrieve hot posts from.
+    :param hot_list: A list to store hot post titles.
+    :param count: The count of posts to skip.
+    :param after: The 'after' parameter for pagination.
+    :return: A list of hot post titles.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Your-User-Agent"}  # Replace with your own User-Agent.
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"count": count, "after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 400:
+    response = requests.get(url, params={"count": count, "after": after}, headers=headers, allow_redirects=False)
+
+    if response.status_code >= 400:
+        print(f"Error: Request failed with status code {response.status_code}")
         return None
 
-    hot_l = hot_list + [child.get("data").get("title")
-                        for child in sub_info.json()
-                        .get("data")
-                        .get("children")]
+    data = response.json()
+    hot_posts = [child['data']['title'] for child in data['data']['children']]
 
-    info = sub_info.json()
-    if not info.get("data").get("after"):
-        return hot_l
+    if not data['data']['after']:
+        return hot_list + hot_posts
+    else:
+        return get_hot_posts(subreddit, hot_list + hot_posts, data['data']['count'], data['data']['after'])
 
-    return recurse(subreddit, hot_l, info.get("data").get("count"),
-                   info.get("data").get("after"))
+if __name__ == "__main__":
+    subreddit = "python"  # Replace with the subreddit you want to query.
+    hot_posts = get_hot_posts(subreddit)
+
+    if hot_posts:
+        for i, post in enumerate(hot_posts, start=1):
+            print(f"{i}. {post}")
+    else:
+        print("No hot posts found.")
